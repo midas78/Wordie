b5d83e164e1141f60a18ebd0a59f9b0748e4424b
midas78 <midas78@wgu.edu>
2026-06-28 21:19:52 -0500

    Structure Changes

--- README.md
+++
@@ -1,2 +0,0 @@
-# Wordie
-Browser-based word puzzle game inspired by NewYork Times Wordle
--- src/main/java/com/wordie/repository/WordRepository.java
+++ src/main/java/com/wordie/repository/WordRepository.java
@@ -1,15 +1,1 @@
 package com.wordie.repository;
-
-import com.wordie.model.Word;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WordRepository extends JpaRepository<Word, Long> {
-
-    boolean existsByWord(String word);
-
-    @Query(value = "SELECT w FROM Word w ORDER BY FUNCTION('RAND')")
-    java.util.List<Word> findRandomWord();
-}
